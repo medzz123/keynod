9762ca4010637af982029446ef4c89faf02883c0
Mahedi Hasan <mahedi.hs1997@gmail.com>
2020-06-28 20:52:51 +0100

    feat(refactor): update types name

--- src/typings/generated.ts
+++ src/typings/generated.ts
@@ -1,5 +1,5 @@
 import { GraphQLResolveInfo } from 'graphql';
-import { MyContext } from './context';
+import { MyContext } from './types';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: any }> = { [K in keyof T]: T[K] };
 export type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };
